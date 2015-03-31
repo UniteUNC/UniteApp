d412a90a325abbc364b60fd0320b9b2eade516f1
Shrivar Ramlakan <shrivar@gmail.com>
2015-03-31 15:36:27 -0400

    Init commit

---
+++ app.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+//Authentication dependencies
+var passport = require('passport')
+var gcal = require('google-calendar');
+var util = require('util');
+var googleStrategy = require('passport-google-oauth2').Strategy;
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
+var session = require('express-session')
+var config = require('./node_modules/google-calendar/specs/config');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+var stormpathMiddleware = stormpath.init(app, {
+    	apiKeyFile: '/Users/Admin/Documents/Comp390/apiKey.properties',
+    	application: 'https://api.stormpath.com/v1/applications/2fZ2OU3JhLWWiHPE8WsECM',
+    	secretKey: 'T2NVglGDiABcKWEwGlUz',
+    	expandCustomData: true,
+    	enableForgotPassword: true
+});
+//Passportjs setup start
+
+  app.use(cookieParser());
+  app.use(bodyParser());
+  app.use(session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+
+
+passport.use(new googleStrategy({
+    clientID: config.consumer_key,
+    clientSecret: config.consumer_secret,
+    callbackURL: "http://localhost:3000/profile",
+    scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    profile.accessToken = accessToken;
+    return done(null, profile);
+  }
+));
+
+app.get('/auth',
+  passport.authenticate('google', { session: false }));
+
+app.get('/auth/callback',
+  passport.authenticate('google', { session: false, failureRedirect: '/login' }),
+  function(req, res) {
+    req.session.access_token = req.user.accessToken;
+    res.redirect('/');
+  });
+//Passportjs setup end
+
+//Google Calandar
+
+app.all('/', function(req, res){
+
+  if(!req.session.access_token) return res.redirect('/auth');
+
+  //Create an instance from accessToken
+  var accessToken = req.session.access_token;
+
+  gcal(accessToken).calendarList.list(function(err, data) {
+    if(err) return res.send(500,err);
+    return res.send(data);
+  });
+});
+
+app.all('/:calendarId', function(req, res){
+
+  if(!req.session.access_token) return res.redirect('/auth');
+
+  //Create an instance from accessToken
+  var accessToken     = req.session.access_token;
+  var calendarId      = req.params.calendarId;
+
+  gcal(accessToken).events.list(calendarId, {maxResults:1}, function(err, data) {
+    if(err) return res.send(500,err);
+
+    console.log(data)
+    if(data.nextPageToken){
+      gcal(accessToken).events.list(calendarId, {maxResults:1, pageToken:data.nextPageToken}, function(err, data) {
+        console.log(data.items)
+      })
+    }
+
+
+    return res.send(data);
+  });
+});
+
+
+app.all('/:calendarId/:eventId', function(req, res){
+
+  if(!req.session.access_token) return res.redirect('/auth');
+
+  //Create an instance from accessToken
+  var accessToken     = req.session.access_token;
+  var calendarId      = req.params.calendarId;
+  var eventId         = req.params.eventId;
+
+  gcal(accessToken).events.get(calendarId, eventId, function(err, data) {
+    if(err) return res.send(500,err);
+    return res.send(data);
+  });
+});
+//Google calendar end
+
+app.use(stormpathMiddleware);
+
+app.get('/', function (req, res) {
+	res.render('home', {
+		title: 'Welcome'
+	});
+});
+
+app.use('/profile',require('./profile')());
+
+app.listen(3000);
