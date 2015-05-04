43ead04f40034cf43610e6b0829731d46f1aa10a
Shrivar Ramlakan <shrivar@gmail.com>
2015-05-04 02:32:00 -0400

    integrated with polymer front-end

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express.io');
 var stormpath = require('express-stormpath');
 var geolocation = require('geolocation');
+var geolib = require('geolib');
+
+var app = express(); //change
+app.use("/googleauth/public",express.static(__dirname + '/views'));
 
 //MongoDB dependencies
 var dbconfig = require('./dblogin');
@@ -24,16 +28,11 @@ var config = require('./node_modules/google-calendar/specs/config');
 // make html, js & css files accessible
 //var files = new static.Server('./public');
 
-var freeBusy;
-var tempcoord = [];
 
-var app = express(); //change
 
 var static = require('node-static');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-
 
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -46,12 +45,14 @@ var stormpathMiddleware = stormpath.init(app, {
       enableForgotPassword: true
 });
 //Passportjs setup start
-
+  
   app.use(bodyParser());
   app.use(session({ secret: 'keyboard cat',
                    }));
   app.use(passport.initialize());
   app.use(passport.session());
+  
+  
 
 
 passport.use(new googleStrategy({
@@ -68,6 +69,15 @@ passport.use(new googleStrategy({
 
 app.use(stormpathMiddleware);
 
+function personData(start,end, username, available, lat, long) {
+    this.start = start;
+	this.end = end;
+	this.username = username;
+	this.available = available;
+	this.lat = lat;
+	this.long = long;
+	//console.log(this);
+};
 
 app.get('/auth',
   passport.authenticate('google', { session: false, successRedirect: '/', failureRedirect: '/' }));
@@ -76,17 +86,22 @@ app.get('/auth/callback',
   passport.authenticate('google', { session: false, failureRedirect: '/login' }),
   function(req, res) {
     req.session.access_token = req.user.accessToken;
-    res.redirect('/googleauth');
+    res.redirect('/');
   });
 //Passportjs setup end
 
 //Google Calandar
 
-app.all('/googleauth',stormpath.loginRequired , function(req, res){
+app.all('/',stormpath.loginRequired , function(req, res){
+	
+	
+	var freeBusy;	
 
   if(!req.session.access_token) return res.redirect('/auth');
   //Create an instance from accessToken
   var accessToken = req.session.access_token;
+	
+	
 
   gcal(accessToken).calendarList.list(function(err, data) {
     if(err) return res.send(500,err);
@@ -115,7 +130,7 @@ app.all('/googleauth',stormpath.loginRequired , function(req, res){
        var currentdateString = currentdate.toISOString();
 
        var onehourdate = new Date();
-       onehourdate.setHours(onehourdate.getHours() + 1);
+       onehourdate.setHours(onehourdate.getHours() + 6);
        var onehourdateString = onehourdate.toISOString();
 	  //change to store in stormpath user custom data instead of global variable
        freeBusy = {
@@ -124,6 +139,8 @@ app.all('/googleauth',stormpath.loginRequired , function(req, res){
          "items": 
          calendarIDobj  
        }
+	   
+	   req.session.freebusy = freeBusy
 
     return res.redirect('/googleauth/getjson');
   });
@@ -138,14 +155,14 @@ app.io.route('ready',function(req) {
 
 app.all('/googleauth/getjson',stormpath.loginRequired , function (req, res){
 
-   //var exist = userdb.UsersList.find({ username : req.user.username }).limit(1);
-   //console.log(exist);
+   var exist = userdb.UsersList.find({ username : req.user.username }).limit(1);
+//   console.log(exist);
 
   if(!req.session.access_token) return res.redirect('/auth');
 
   var accessToken = req.session.access_token;
 
-  gcal(accessToken).freebusy.query(freeBusy,function(err, data) {
+  gcal(accessToken).freebusy.query(req.session.freebusy,function(err, data) {
         if(err) return res.send(500,err);
 
        
@@ -157,8 +174,8 @@ app.all('/googleauth/getjson',stormpath.loginRequired , function (req, res){
         db.UsersFreeBusy.remove({ username : req.user.username },
 
           function(err, doc) {
-          //if(err != null)
-            //console.log(err)
+          if(err)
+            console.log(err)
             
         }); 
 
@@ -166,15 +183,22 @@ app.all('/googleauth/getjson',stormpath.loginRequired , function (req, res){
         db.UsersFreeBusy.insert(
         data, function(err, doc) {
 
-          //if(err != null)
-            //console.log(err)
-            
+          if(err)
+            console.log(err)
+			
+		//return res.send(data);	
+		var info = parseFriends(req,res);
+		//return res.sendfile(__dirname + '/views/index.html');
+			
+          
+  	});  
+	  
+	  	
         });
-
-
-
-        return res.redirect('/');
-      });
+	
+	
+	
+	
 
 });
 
@@ -182,20 +206,31 @@ app.all('/display',stormpath.loginRequired , function (req, res) {
  
 
 
-  parseFriends(req,res)
+ var info = parseFriends(req,res)
+ 
+//display data based on the info array of persondata objects
+ //calc distance
+ 
 
-  res.render('home', {
-    title: 'Welcome'
-  });
+
+	//res.send(info)
+	
+//  res.render('home', {
+//    title: 'Welcome'
+//  });
 });
 
-app.get('/', function (req, res) {
+//app.get('/',stormpath.loginRequired, function (req, res) {
+	
+  //res.sendfile(__dirname + '/views/index.html');
   	
-  res.render('home', {
-    title: 'Welcome',
-	username: JSON.stringify(req.user.username)
-  });
-});
+	//res.render('index.html');
+	
+//  res.render('home', {
+//    title: 'Welcome',
+//	username: JSON.stringify(req.user.username)
+//  });
+//});
 
 
 
@@ -206,35 +241,41 @@ io.set('origins', '*:*');
 
 function parseFriends(req,res)
 {
+  
   var starttimes = [];
-  var endtimes = []
-  var times = [starttimes, endtimes]
+  var endtimes = [];
+	
+  
+  var peopleData = [];
 
   var friendsString = req.user.customData.friends
   var friendsArray = friendsString.split(",")
+  var friendIndex = 0;
 
   var currentdate = new Date();
 
   //Date.parse(datestring)
-
-  for(index in friendsArray)
-  {
-      //Query is asynchronous
-      db.UsersFreeBusy.find({username : friendsArray[index]}).forEach(function(err, doc) 
+ 
+	
+  asyncLoop(friendsArray.length , function(loop) {
+    //Query is asynchronous
+      db.UsersFreeBusy.find({username : friendsArray[friendIndex]}).forEach(function(err, doc) 
       {
-
+		  starttimes = [];
+		  endtimes = []
+		  
         if(err)
           console.log(err + "err")
           
 
-        if (!doc) 
+        if (!doc || doc == null) 
         {
         // we visited all docs in the collection
         return;
-        }
-
+        }  
           
         var jsonretrieve = doc;
+		  
         jsonretrieve.calendars = JSON.parse(jsonretrieve.calendars)   
         //Loop to iterate through the JSON object and pick out busy times from the json object
         for (var key in jsonretrieve.calendars) 
@@ -245,8 +286,10 @@ function parseFriends(req,res)
             {
                 
               var busyItems = jsonretrieve.calendars[key]
+			 
+			  //console.log(busyItems + "BUSYITEMS");
               var timeItems = busyItems.busy[0]
-                
+              //console.log(timeItems);
               if(timeItems)
               {
 
@@ -258,15 +301,109 @@ function parseFriends(req,res)
             }
           }
             
-        }  
+        } 
+		  
+		  //determine if the user is currently busy, or when they will become available (if they are busy), otherwise, say available until _
+		  
+		  //if user if available for the foreseeable future (6 hours)
+		  if(starttimes.length == 0)
+		  {
+			  peopleData[peopleData.length] = new personData(0,0,jsonretrieve.username, "true", jsonretrieve.coordlat, jsonretrieve.coordlong);
+		  }
+				
+		  else
+		  {
+			  var minstart;  //= starttimes[0];
+			  var minend;//= endtimes[0];
+			  var busy = null;
+			  for (j = 0; j < starttimes.length;j++)
+			  {
+				  
+				  if(starttimes[j] < currentdate && endtimes[j] > currentdate)
+						  busy = "true";
+				  
+				  if(starttimes[j] > currentdate && busy != "true")
+					  busy = "false";
+				  
+				 
+					  
+				  if(typeof(minstart) === 'undefined')
+				  {
+					  minstart = starttimes[j];
+					  
+					  if(starttimes[j] < currentdate && endtimes[j] > currentdate)
+						  busy = "true";
+				  }
+				  
+				  
+				  if ((starttimes[j] < minstart && endtimes[j] < minend) || (starttimes[j] < currentdate && enditmes[j] < currentdate))
+					  minstart = starttimes[j];
+			  }
+			  
+			  for (k = 0; k < endtimes.length;k++)
+			  {
+				  if (endtimes[k] < minend || (starttimes[j] < currentdate && enditmes[j] < currentdate))
+					  minend = endtimes[k];
+				  
+				  if(typeof(minend) === 'undefined')
+				  {
+					  minend = endtimes[k]
+				  }
+			  }
+			  
+			  
+			  
+			 
+				  peopleData[peopleData.length] = new personData(minstart,minend,jsonretrieve.username, busy, jsonretrieve.coordlat, jsonretrieve.coordlong);
+			
+		  }
 
-        //console.log(times)1 
+        
 
         //Need to pass this information somewhere in order to create a display for availability for this friend.
-      
-      });
+		friendIndex++;
+      	loop.next()
+      })},
+    function(){return res.render('index', {
+	    username: req.session.username,
+		data: JSON.stringify(peopleData)
+      });}
+ 
+);
 
-  } 
+}
 
-  
-};
+			
+
+function asyncLoop(iterations, func, callback) {
+    var index = 0;
+    var done = false;
+    var loop = {
+        next: function() {
+            if (done) {
+                return;
+            }
+
+            if (index < iterations) {
+                index++;
+                func(loop);
+
+            } else {
+                done = true;
+                callback();
+            }
+        },
+
+        iteration: function() {
+            return index - 1;
+        },
+
+        break: function() {
+            done = true;
+            callback();
+        }
+    };
+    loop.next();
+    return loop;
+}
+
