dff05662fed774af81fd11e44f373481b36f12c2
Shrivar Ramlakan <shrivar@gmail.com>
2015-05-04 22:48:05 -0400

    change server ports

--- app.js
+++ app.js
@@ -260,12 +260,12 @@ app.all('/display',stormpath.loginRequired , function (req, res) {
 
 app.use('/profile',require('./profile')());
 
-app.listen(3000);
+//app.listen(3000);
 //Openshift deployment
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 // 
-//app.listen(server_port, server_ip_address)
+app.listen(server_port, server_ip_address)
 
 io.set('origins', '*:*');
 
